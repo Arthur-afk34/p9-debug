df8d912054ee8b1fe0e093f825d1653d3f8b8d64
Arthur-afk34 <seigneurarthur974@gmail.com>
2024-10-15 12:07:30 +0400

    8 test no bug changer le slider indexjstest

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,36 +1,3 @@
-// import React from "react";
-// import { render, screen, fireEvent } from "@testing-library/react";
-// import Form from "./index";
-
-// describe("When Form is created", () => {
-//   it("a list of fields card is displayed", async () => {
-//     render(<Form />);
-
-//     await screen.findByText("Nom");
-//     await screen.findByText("Prénom");
-//     await screen.findByText("Personnel / Entreprise");
-//     await screen.findByText("Email");
-//     await screen.findByTestId("button-test-id");
-//   });
-
-//   describe("and a click is triggered on the submit button", () => {
-//     it("the success action is called", async () => {
-//       const onSuccess = jest.fn();
-//       render(<Form onSuccess={onSuccess} />);
-//       fireEvent(
-//         await screen.findByTestId("button-test-id"),
-//         new MouseEvent("click", {
-//           cancelable: true,
-//           bubbles: true,
-//         })
-//       );
-//       await screen.findByText("En cours");
-//       await screen.findByText("Envoyer");
-//       expect(onSuccess).toHaveBeenCalled();
-//     });
-//   });
-// });
-
 import { fireEvent, render, screen } from "@testing-library/react";
 import Form from "./index";
 
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,60 +12,6 @@ export const getSortedData = () => {
   );
 };
 
-// const Slider = () => {
-//   const [index, setIndex] = useState(0);
-
-//   const byDateDesc = getSortedData();
-
-//   const nextCard = () => {
-//     setTimeout(() => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), 5000);
-//   };
-
-// //  ajout de la fonction  handleRadioChange pour gérer le changement de radio
-//  const handleRadioChange = (newIndex) => {
-//   setIndex(newIndex);
-// };
-
-//   useEffect(() => {
-//     nextCard();
-//     // console.log(byDateDesc);
-//   });
-//   // console.log(byDateDesc);
-//   return (
-//     <div className="SlideCardList">
-//       {byDateDesc?.map((event, idx) => (
-//         <div key={event.id} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
-//           <img src={event.cover} alt="forum" />
-//           <div className="SlideCard__descriptionContainer">
-//             <div className="SlideCard__description">
-//               <h3>{event.title}</h3>
-//               <p>{event.description}</p>
-//               <div>{getMonth(new Date(event.date))}</div>
-//             </div>
-//           </div>
-//         </div>
-//       ))}
-//       <div className="SlideCard__paginationContainer">
-//         <div className="SlideCard__pagination">
-//           {byDateDesc?.map((event, radioIdx) => (
-            
-//             <input
-//               key={event.id} // Utilisation de  l'ID de l'événement focus comme clé unique
-//               type="radio"
-//               name="radio-button"
-//               checked={index === radioIdx}
-//               onChange={() => handleRadioChange(radioIdx)} // Ajout  d'un gestionnaire onChange
-//             />
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-  
-// };
-
-// export default Slider;
-
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
